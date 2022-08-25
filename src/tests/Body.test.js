02a706fdde38bb52b362279d76cb758c6b0fd751
Caleb Craigo <CalebDOS1@yahoo.com>
2022-08-25 18:14:49 -0400

    Updated test to for redux

--- src/tests/Body.test.js
+++ src/tests/Body.test.js
@@ -2,12 +2,17 @@ import React from 'react';
 import Enzyme, { shallow, mount } from 'enzyme';
 import Body from '../components/Body.jsx';
 import Adapter from 'enzyme-adapter-react-16';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+const mockStore= configureMockStore();
+const store = mockStore({});
+
 describe('Body component', () => {
     test('renders', () => {
-        const wrapper = shallow(<Body />);
+        const wrapper = shallow(<Provider store={store}> <Body /> </Provider>);
 
         expect(wrapper.exists()).toBe(true);
     })
--- src/tests/CreateOrder.test.js
+++ src/tests/CreateOrder.test.js
@@ -2,12 +2,17 @@ import React from 'react';
 import Enzyme, { shallow, mount } from 'enzyme';
 import CreateOrder from '../components/CreateOrder.jsx';
 import Adapter from 'enzyme-adapter-react-16';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+const mockStore = configureMockStore();
+const store = mockStore({});
+
 describe('CreateOrder component', () => {
     test('renders', () => {
-        const wrapper = shallow(<CreateOrder open={true}/>);
+        const wrapper = shallow(<Provider store={store}><CreateOrder open={true}/></Provider>);
 
         expect(wrapper.exists()).toBe(true);
     })
