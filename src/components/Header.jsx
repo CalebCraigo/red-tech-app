c8cba0cada17037dec633c53218e01bf48f09ac6
Caleb Craigo <CalebDOS1@yahoo.com>
2022-08-25 00:03:10 -0400

    Red Tech logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,12 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-import MenuList from '@mui/material/MenuList';
-import MenuItem from '@mui/material/MenuItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Paper from '@mui/material/Paper';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import SettingsIcon from '@mui/icons-material/Settings';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import MenuIcon from '@mui/icons-material/Menu';
+import redTechnologiesSquarelogo from '../static/redTechnologiesSquarelogo.png';
+
+import '../styles/header.css';
 
 const Header = () => {
     const theme = createTheme({
@@ -44,7 +41,7 @@ const Header = () => {
                 <AppBar position="static">
                     <Toolbar>
                         <IconButton edge="start" color="inherit" aria-label="menu" >
-                            <MenuIcon />
+                            <img className="logo" src={redTechnologiesSquarelogo} alt="Red Technologies" />
                         </IconButton>
                         <Typography edge="start" sx={{ fontFamily: 'sans-serif', flexGrow: 1 }} color="808080" component="div">
                             Home
