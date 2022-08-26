3df43e7ffc882dcb18bee74e15c47d269e19cc06
Caleb Craigo <CalebDOS1@yahoo.com>
2022-08-26 15:19:08 -0400

    server update

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, '..', 'red-tech-app/build');
 
-app.use(express.static(publicPath));
+// const publicPath = path.join(__dirname, '..', 'red-tech-app/build');
 
-app.get('*', (req, res) => {
-   res.sendFile(path.join(__dirname, 'build/index.html'));
-});
-
+if(process.env.NODE_ENV ==="production") {
+   app.use(express.static('build'));
+   app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+   });
+}
 app.listen(port, () => {
    console.log(`Server is up on port ${port}!`);
 });
