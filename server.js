252d89406a91b7bb6f81b35834ce5be0e9ee49a1
Caleb Craigo <CalebDOS1@yahoo.com>
2022-08-25 21:52:42 -0400

    updated sent file

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, '..', 'red-tech-app');
+const publicPath = path.join(__dirname, '..', 'red-tech-app/src');
 
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
+   res.sendFile(path.join(publicPath, 'index.js'));
 });
 
 app.listen(port, () => {
