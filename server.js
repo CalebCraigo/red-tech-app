7394a961e3940d9c3da60eb0400826e6fceb5bd1
Caleb Craigo <CalebDOS1@yahoo.com>
2022-08-26 09:54:38 -0400

    updating server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const publicPath = path.join(__dirname, '..', 'red-tech-app/src');
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.js'));
+   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
 app.listen(port, () => {
