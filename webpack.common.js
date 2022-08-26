47193746011236b3483bf7eea4c44b1941cf38a4
Caleb Craigo <CalebDOS1@yahoo.com>
2022-08-26 11:52:39 -0400

    add html to build

--- webpack.common.js
+++ webpack.common.js
@@ -1,31 +1,35 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  entry: "/src/index.js",
-  output: { path: path.resolve(__dirname, "dist") },
-  module: {
-    rules: [
-        {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-            loader: "babel-loader",
-            options: {
-                presets: ["@babel/preset-env", "@babel/preset-react"],
-            },
-            },
-        },
-        {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-        },
-        { 
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
+    entry: "/src/index.js",
+    output: { path: path.resolve(__dirname, "dist") },
+    module: {
+        rules: [
             {
-                loader: 'file-loader',
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env", "@babel/preset-react"],
+                },
+                },
             },
-            ],
-        },
-    ],
-  },
+            {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"]
+            },
+            { 
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [
+                {
+                    loader: 'file-loader',
+                },
+                ],
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin()
+    ]
 };
