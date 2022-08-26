20d62f94441e16b68618b77a18e89fa0f9047cc6
Caleb Craigo <CalebDOS1@yahoo.com>
2022-08-26 10:53:20 -0400

    updating package.json

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Main from './components/Main.jsx';
 import {Provider} from 'react-redux'
-import store from './redux/store'
+import store from './redux/store.js'
 
 function App() {
   return (
